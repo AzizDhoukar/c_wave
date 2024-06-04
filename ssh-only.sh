322170fabf773e7bf4b44db1f9328f9ea1a26785
Aziz Dhoukar <aziz.dhoukar@gmail.com>
2024-06-04 18:02:34 +0100

    done

--- ssh-only.sh
+++ ssh-only.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 scp docker-compose.yml user@server:~/docker/docker-compose.yml
 scp docker-images.tar user@server:~/docker/docker-images.tar
-ssh user@server "docker load -i /path/to/your-image.tar"
+ssh -i "ssh-key.pom" user@server
+docker load -i ~/docker/docker-images.tar
+docker-compose -f ~/docker/docker-compose.yml up 
